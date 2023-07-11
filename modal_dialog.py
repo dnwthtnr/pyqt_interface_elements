03a45d4e057d4e6a07f9f251bed6000b45b6c79a
dnwthtnr <dunworthtanner@gmail.com>
2023-07-10 23:18:18 -0400

    file selecting dialog

--- modal_dialog.py
+++ modal_dialog.py
@@ -2,7 +2,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-from pyqt_interface_elements import base_widgets, icons, base_windows
+from pyqt_interface_elements import base_widgets, icons, base_windows, line_edits
 from PySide2 import QtCore
 
 
@@ -10,5 +10,7 @@ class ChooseFilePath(base_windows.Dialog):
 
     def __init__(self):
         super().__init__()
+
+        self.file_path_button = line_edits.Folder_Selection_Line_Edit()
 
 
