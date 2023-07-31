cd764b7067d65f4c7f305185f3f6007857d54697
dnwthtnr <dunworthtanner@gmail.com>
2023-07-31 17:30:05 -0400

    slider ticks in progress

--- slider.py
+++ slider.py
@@ -2,10 +2,8 @@ from PySide2 import QtCore, QtWidgets, QtGui
 from pyqt_interface_elements import base_widgets, constants
 
 
-
 class RangeSlider(base_widgets.Slider):
     sliderMoved = QtCore.Signal(int, int)
-
 
     def __init__(self, minimum, maximum):
         super().__init__()
@@ -16,7 +14,6 @@ class RangeSlider(base_widgets.Slider):
 
         self._lower_bound = 50
         self._upper_bound = self.maximum()
-
 
     # region Value Setters
 
@@ -34,40 +31,93 @@ class RangeSlider(base_widgets.Slider):
         self._upper_bound = value
         self.update()
 
-
     # endregion
 
     def paintEvent(self, event):
-
-
         painter = QtGui.QPainter(self)
         style = QtWidgets.QApplication.style()
 
+        _groove_control_rect = self.paintSliderGroove(painter, style)
 
-        for i, _handle_value in enumerate([self._lower_bound, self._upper_bound]):
-            opt = QtWidgets.QStyleOptionSlider()
-            self.initStyleOption(opt)
+        # self.paintTickMarks(_groove_control_rect, painter, style)
 
-            # Only draw the groove for the first slider so it doesn't get drawn
-            # on top of the existing ones every time
-            if i == 0:
-                opt.subControls = QtWidgets.QStyle.SC_SliderHandle# | QtWidgets.QStyle.SC_SliderGroove
-            else:
-                opt.subControls = QtWidgets.QStyle.SC_SliderHandle
-
-            if self.tickPosition() != self.NoTicks:
-                opt.subControls |= QtWidgets.QStyle.SC_SliderTickmarks
-
-            # if self.pressed_control:
-            #     opt.activeSubControls = self.pressed_control
-            # else:
-            #     opt.activeSubControls = self.hover_control
-
-            opt.sliderPosition = _handle_value
-            opt.sliderValue = _handle_value
-            style.drawComplexControl(QtWidgets.QStyle.CC_Slider, opt, painter, self)
-
+        self.paintSliderHandles(painter=painter, style=style)
         return
+
+
+    # region Painters
+    def paintSliderHandle(self, handle_value):
+        """
+        Paints a slider handle at the given value
+
+        Parameters
+        ----------
+        handle_value : float
+            The value to draw the slider handle at
+
+        Returns
+        -------
+        QtWidgets.QStyleOptionSlider
+            A slider style option defining the slider handle
+
+        """
+        opt = QtWidgets.QStyleOptionSlider()
+        self.initStyleOption(opt)
+
+        # Only draw the groove for the first slider so it doesn't get drawn
+        # on top of the existing ones every time
+        opt.subControls = QtWidgets.QStyle.SC_SliderHandle
+
+        # if self.tickPosition() != self.NoTicks:
+        #     opt.subControls |= QtWidgets.QStyle.SC_SliderTickmarks
+
+        opt.sliderPosition = handle_value
+        opt.sliderValue = handle_value
+        return opt
+
+    def paintSliderHandles(self, painter, style):
+        """
+        Draws the handles for the upper and lower bound
+
+        Parameters
+        ----------
+        painter : QtGui.QPainter
+            TYhe painter to be used to draw teh slider handles
+        style : QtWidgets.QStyle
+            The style for the current QApplication
+
+        """
+        for i, _handle_value in enumerate([self._lower_bound, self._upper_bound]):
+            _slider_handle_style_option = self.paintSliderHandle(handle_value=_handle_value)
+            style.drawComplexControl(QtWidgets.QStyle.CC_Slider, _slider_handle_style_option, painter, self)
+
+
+    def paintSliderGroove(self, painter, style):
+        _opt = QtWidgets.QStyleOptionSlider()
+        self.initStyleOption(_opt)
+
+        _opt.sliderValue = 0
+        _opt.sliderPosition = 0
+        _opt.subControls = QtWidgets.QStyle.SC_SliderGroove
+        self.setTickPosition(self.TickPosition)
+        _opt.subControls = QtWidgets.QStyle.SC_SliderTickmarks
+
+        style.drawComplexControl(QtWidgets.QStyle.CC_Slider, _opt, painter, self)
+
+        # _control_rect = style.subControlRect(QtWidgets.QStyle.CC_Slider, _opt, QtWidgets.QStyle.SC_SliderGroove, self)
+        # return _control_rect
+
+
+
+    def paintTickMarks(self, slider_groove_rect, painter, style):
+        _opt = QtWidgets.QStyleOptionSlider()
+        self.initStyleOption(_opt)
+        _opt.subControls = QtWidgets.QStyle.SC_SliderTickmarks
+        _opt.tickInterval = 1
+
+        style.drawComplexControl(QtWidgets.QStyle.CC_Slider, _opt, painter, self)
+        return
+    # endregion
 
     def mousePressEvent(self, event):
         event.accept()
@@ -79,26 +129,23 @@ class RangeSlider(base_widgets.Slider):
             event.ignore()
             return
 
-
         _slider_style_option = QtWidgets.QStyleOptionSlider()
         self.initStyleOption(_slider_style_option)
 
         self.active_slider = -1
 
-        for i, slider_value in enumerate([self._lower_bound, self._upper_bound]):
+        for slider_index, slider_value in enumerate([self._lower_bound, self._upper_bound]):
             _slider_style_option.sliderPosition = slider_value
-            _element_clicked_on = _widget_style.hitTestComplexControl(_widget_style.CC_Slider, _slider_style_option, event.pos(), self)
+            _element_clicked_on = _widget_style.hitTestComplexControl(_widget_style.CC_Slider, _slider_style_option,
+                                                                      event.pos(), self)
 
             if _element_clicked_on == _widget_style.SC_SliderHandle:
-                self.active_slider = i
+                self.active_slider = slider_index
                 self._pressed_element = _element_clicked_on
 
                 self.triggerAction(self.SliderMove)
                 self.setRepeatAction(self.SliderNoAction)
                 self.setSliderDown(True)
-
-                print(self.active_slider)
-
                 break
 
     def mouseMoveEvent(self, event):
@@ -106,9 +153,7 @@ class RangeSlider(base_widgets.Slider):
             event.ignore()
             return
 
-
         event.accept()
-        print(event.pos())
         _new_position = self.pixel_value_to_slider_value(event.pos())
 
         if self.active_slider == 0:
@@ -168,9 +213,6 @@ class RangeSlider(base_widgets.Slider):
         return _slider_value
 
 
-
-
-
 if __name__ == "__main__":
     import sys
 
@@ -185,6 +227,3 @@ if __name__ == "__main__":
     _window.show()
 
     sys.exit(_app.exec_())
-    import time
-    time.sleep(2.5)
-    _window.setLowerBound(50)
