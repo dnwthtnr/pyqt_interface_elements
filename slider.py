0a4ce33126d398d2c5cc3fde730bdb2d2b471af7
dnwthtnr <dunworthtanner@gmail.com>
2023-08-04 13:50:22 -0400

    Functioning as intended. Cleanup needed

--- slider.py
+++ slider.py
@@ -15,9 +15,11 @@ class RangeSliderTick(QtWidgets.QWidget):
 class RangeSlider(base_widgets.Slider):
     sliderMoved = QtCore.Signal(int, int)
 
-    def __init__(self, minimum, maximum):
+    def __init__(self, minimum, maximum, values_to_mark=[]):
         super().__init__()
         self.setOrientation(constants.horizontal)
+
+        self._marked_values = values_to_mark
 
         self.setMinimum(minimum)
         self.setMaximum(maximum)
@@ -25,9 +27,66 @@ class RangeSlider(base_widgets.Slider):
         self._lower_bound = 50
         self._upper_bound = self.maximum()
 
-        self.tick_marks = []
+        self._pressed_element = None
 
     # region Value Setters
+
+    def setValuesToMark(self, values):
+        """
+        Set values to paint markers at
+
+        Parameters
+        ----------
+        values : list[int] or list[float]
+
+        Returns
+        -------
+
+        """
+        if not isinstance(values, list):
+            return
+        for _value in values:
+            if not isinstance(_value, int) and not isinstance(_value, float):
+                return
+
+        self._marked_values = values
+
+    def valuesToMark(self):
+        """
+        Values to paint markers at
+
+        Returns
+        -------
+
+        """
+        return self._marked_values
+
+    def addValueToMark(self, value):
+        """
+        Adds a value to paint a marker at
+
+        Parameters
+        ----------
+        value
+
+        Returns
+        -------
+
+        """
+        self._marked_values.append(value)
+
+    def removeValueToMark(self, value):
+        """
+        Removes the given value from the values to paint markers at if it is present
+
+        Parameters
+        ----------
+        value : int or float
+
+        """
+        if value in self.valuesToMark():
+            _index = self._marked_values.index(value)
+            self._marked_values.pop(_index)
 
     def lowerBound(self):
         return self._lower_bound
@@ -51,13 +110,14 @@ class RangeSlider(base_widgets.Slider):
 
         # self.paint
 
-        self.paintSliderGroove(painter, style)
+        # self.paintSliderGroove(painter, style)
 
         # self.paintTickMarks(_groove_control_rect, painter, style)
 
         self.paintSelectedRange(painter, style)
+        self.paintTickMarks(painter, style)
         self.paintSliderHandles(painter=painter, style=style)
-        self.paintRandomTickMarks(painter, style)
+        self.paintMarkers(painter, style)
         return
 
     # region Painters
@@ -149,34 +209,47 @@ class RangeSlider(base_widgets.Slider):
         _bottom_right   = QtCore.QPoint(_pixel_upper_limit, _slider_bottom)
 
         _selection_rectangle = QtCore.QRect(_top_left, _bottom_right)
+
+        _color = QtGui.QColor(155, 155, 155, 255)
+
+        painter.fillRect(_selection_rectangle, _color)
         # _selection_rectangle = QtCore.QRect(QtCore.QPoint(0, 100), QtCore.QPoint(500, 0))
 
-        _pen = QtGui.QPen()
-        _pen.setColor(QtCore.Qt.blue)
-
-        _brush = QtGui.QBrush()
-        _brush.setColor(QtCore.Qt.blue)
-        # _brush
-
-        painter.setPen(_pen)
-        painter.setBrush(_brush)
-
-        _opt = QtWidgets.QStyleOptionSlider()
-        self.initStyleOption(_opt)
-        _opt.subControls = QtWidgets.QStyle.SC_SliderGroove
-        # print(_opt.rect)
-        _opt.rect = _selection_rectangle
+        # _pen = QtGui.QPen()
+        # _pen.setColor(QtCore.Qt.blue)
+        #
+        # _brush = QtGui.QBrush()
+        # _brush.setColor(QtCore.Qt.blue)
+        # # _brush
+        #
+        # painter.setPen(_pen)
+        # painter.setBrush(_brush)
+        #
+        # _opt = QtWidgets.QStyleOptionSlider()
+        # self.initStyleOption(_opt)
+        # _opt.subControls = QtWidgets.QStyle.SC_SliderGroove
+        # # print(_opt.rect)
+        # _opt.rect = _selection_rectangle
         # print(_selection_rectangle)
 
 
         #TODO: style option for rectangle and set it as the region. then draw with the given style and painter
 
-        style.drawComplexControl(QtWidgets.QStyle.CC_Slider, _opt, painter, self)
+        # style.drawComplexControl(QtWidgets.QStyle.CC_Slider, _opt, painter, self)
 
-    def paintRandomTickMarks(self, painter, style):
-        # _style = QtWidgets.QStyle()
-        # _style.
-        for i, _handle_value in enumerate([4, 50, 75, 20, 9, 44]):
+    def paintMarkers(self, painter, style):
+        """
+        Paints markers
+        Parameters
+        ----------
+        painter
+        style
+
+        Returns
+        -------
+
+        """
+        for _handle_value in self.valuesToMark():
             _pixel_value = self.slider_value_to_pixel_value(_handle_value)
             _top = self.rect().top()
 
@@ -185,14 +258,40 @@ class RangeSlider(base_widgets.Slider):
 
             _rect = QtCore.QRect(_top_left, _bottom_right)
 
-            _color = QtGui.QColor(255, 10, 20, 50)
+            _color = QtGui.QColor(255, 10, 20, 130)
 
             painter.fillRect(_rect, _color)
 
 
     def paintTickMarks(self, painter, style):
-        _slider_beginning = self.point_dimension_for_orientation(self.rect().left())
-        _slider_end = self.point_dimension_for_orientation(self.rect().right())
+        _multiple_of = 5
+        _draw_number = False
+        for _value in range(self.maximum()-self.minimum()):
+
+            _result = _value / _multiple_of
+            if not _result.is_integer():
+                continue
+
+            _pixel_value = self.slider_value_to_pixel_value(_value)
+            _top = self.rect().center().y()
+
+
+            _top_left = QtCore.QPoint(_pixel_value, _top)
+            _bottom_right = QtCore.QPoint(_pixel_value + 3, self.rect().bottom())
+
+            _rect = QtCore.QRect(_top_left, _bottom_right)
+
+            _color = QtGui.QColor(100, 100, 100, 255)
+
+            painter.fillRect(_rect, _color)
+
+            if _draw_number is False:
+                _draw_number = True
+                continue
+
+            painter.drawText(QtCore.QPoint(_pixel_value+5, _top-5), str(_value))
+            _draw_number = False
+
     # endregion
 
     def mousePressEvent(self, event):
